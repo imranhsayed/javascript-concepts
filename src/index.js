4e1eb940c1806e29155f374e5413a387096f44a9
Imran Sayed <imranhsayed@gmail.com>
2019-05-30 01:07:55 +0530

    Add async await example

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 ( function () {
 
+	// Promise Example
+
+	console.warn( 'First' );
+	console.warn( 'Second' );
+
+	const newPromise = new Promise( ( resolve, reject ) => {
+		setTimeout( () => {
+			resolve( 'response received' );
+		},2000 )
+	} );
+
+	newPromise.then( res => {
+		// this will be executed after 2 seconds
+		console.warn( 'Third' );
+	} );
+
+	console.warn( 'Fourth' );
+
 } )();
