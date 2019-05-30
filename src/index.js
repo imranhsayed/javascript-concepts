91faaedae6f9c3346fb457006692f92232d7b61a
Imran Sayed <imranhsayed@gmail.com>
2019-05-30 12:14:41 +0530

    Add example for promise.all

--- src/index.js
+++ src/index.js
@@ -15,4 +15,28 @@
 		console.warn( result );
 	} );
 
+	/**
+	 * Promise.all()
+	 *
+	 * Send all request at one time
+	 */
+	const promise1 = Promise.resolve(3);
+	const promise2 = 42;
+	const promise3 = new Promise( ( resolve, reject ) => {
+		setTimeout( () => {
+			resolve( 'Third: response received' );
+		},2000 )
+	} );
+
+	async function getABC() {
+		// Promise.all() allows us to send all requests at the same time.
+		let results = await Promise.all([promise1, promise2, promise3])
+
+		return results;
+	}
+
+	getABC().then( result => {
+		console.warn( result );
+	} )
+
 } )();
