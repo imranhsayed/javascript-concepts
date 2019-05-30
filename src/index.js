eea663bc7f9b6d5764df00ebf5f33dac4760e72e
Imran Sayed <imranhsayed@gmail.com>
2019-05-30 11:00:58 +0530

    Update axios package

--- src/index.js
+++ src/index.js
@@ -2,36 +2,10 @@ import axios from 'axios';
 ( function () {
 
 	/**
-	 * Promise Example 1
+	 * Async Await Example
 	 */
-	console.warn( 'First' );
-	console.warn( 'Second' );
-
-	const newPromise = new Promise( ( resolve, reject ) => {
-		setTimeout( () => {
-			resolve( 'Third: response received' );
-		},2000 )
-	} );
-
-	newPromise.then( value => {
-		// this will be executed after 2 seconds
-		console.warn( value );
-	} );
-
-	console.warn( 'Fourth' );
-
-	/**
-	 * Promise Example 2
-	 */
-	const todosPromise = new Promise( ( resolve, reject ) => {
-		axios.get( 'https://jsonplaceholder.typicode.com/todos/' )
-			.then( res => {
-				resolve( res.data );
-			} );
-	} );
-
-	todosPromise.then( value => {
-		console.warn( value );
-	} );
+	async function getTodoData() {
+		const todoData = await axios.get( '' )
+	}
 
 } )();
