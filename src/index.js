f22c85b891369285c1e0f36eb05e1129b97b2559
Imran Sayed <imranhsayed@gmail.com>
2019-05-30 10:50:00 +0530

    Add another example for promise

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
+import axios from 'axios';
 ( function () {
 
-	// Promise Example
-
+	/**
+	 * Promise Example 1
+	 */
 	console.warn( 'First' );
 	console.warn( 'Second' );
 
 	const newPromise = new Promise( ( resolve, reject ) => {
 		setTimeout( () => {
-			resolve( 'response received' );
+			resolve( 'Third: response received' );
 		},2000 )
 	} );
 
-	newPromise.then( res => {
+	newPromise.then( value => {
 		// this will be executed after 2 seconds
-		console.warn( 'Third' );
+		console.warn( value );
 	} );
 
 	console.warn( 'Fourth' );
+
+	/**
+	 * Promise Example 2
+	 */
+	const todosPromise = new Promise( ( resolve, reject ) => {
+		axios.get( 'https://jsonplaceholder.typicode.com/todos/' )
+			.then( res => {
+				resolve( res.data );
+			} );
+	} );
+
+	todosPromise.then( value => {
+		console.warn( value );
+	} );
 
 } )();
